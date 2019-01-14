5d6c69b7d4b3319a03883d6f534c3688ba309c39
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-14 01:06:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+  array.any? do |item|
+    item.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
