27a8db176190338c1710a59600ea3b9442fb5c58
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-14 01:10:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,9 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    array.find(cheese)
+  end
 end
